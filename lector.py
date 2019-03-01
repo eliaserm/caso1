161f173272b242e13d149fd2fb51d9bac760142e
elias reynoso <eliaserm456@gmail.com>
2019-03-01 13:40:49 -0600

    solucion casa todo meter casas base de datos 1

--- lector.py
+++ lector.py
@@ -1,46 +1,60 @@
 import glob
 import json
 import mysql.connector
-conexion = mysql.connector.connect(user='erm',password ='1234567',database = 'casas')
+
+conexion = mysql.connector.connect(user ='erm', password='1234567', database='casas')
+
 cursor = conexion.cursor()
+
 files = glob.glob('*.json')
-tipo = {'venta':1,'renta':2}
-origen={'informador':1}
+
+tipo = {'venta': 1, 'renta': 2}
+origen = {'informador': 1}
+
 for file in files:
-    with open(file) as f:
+    with open(file, encoding = 'utf-8') as f:
         casas = json.load(f)
-        #print(casas)
         for casa in casas:
-           #print(casa['ubicacion'])
-            select = 'SELECT * FORM municipio WHERE nombre = %s'
-            cursor.execute(select,(casa['ubicacion'],))
+            # print(casa['ubicacion'])
+            select = 'SELECT * FROM municipio WHERE nombre = %s'
+            cursor.execute(select, (casa['ubicacion'], ))
             if len(cursor.fetchall()) == 0:
-                 insert = 'INSERT INTO municipios(nombre) VALUES (%s)'
-                 cursor.execute(insert,(casa['ubicacion'],))
-                 conexion.commit()
+                insert = 'INSERT INTO municipio(nombre) VALUES(%s)'
+                cursor.execute(insert, (casa['ubicacion'], ))
+                conexion.commit()
+                id_muni = cursor.lastrowid
 
-                 id_muni = cursor.lastrowid
-                 insert = 'INSERT INTO colonia(nombre,id_municipio) VALUES (%s,%s)'
-                 cursor.execute(insert, (casa['colonia'],id_muni))
-                 id_colonia = cursor.lastrowid
-                 conexion.commit()
+                insert = 'INSERT INTO colonia(nombre, id_municipio) VALUES(%s, %s)'
+                cursor.execute(insert, (casa['colonia'], id_muni))
+                conexion.commit()
+                id_colonia = cursor.lastrowid
 
-                 select = 'SELECT * FORM fecha WHERE fecha = %s'
-                 cursor.execute(select, (file[:-5],))
-                 if_fecha = 0
-                 row = cursor.fetchall();
-                 if len(cursor.fetchall()==0):
-                     insert = 'INSERT INTO fecha(fecha) VALUES (%s)'
-                     cursor.execute(insert, (file[:-5]))
-                     conexion.commit()
-                     print(cursor.lastrowid)
-                     id_fecha = cursor.lastrowid
-                 else:
-                     if_fecha = row[0][0]
-                     insert = 'INSERT INTO bienraiz(titulo,precio,m2,rooms,baths,cars,descripcion,id_tipo,id_origen,id_colonia,id_fecha) VALUES (%s,%s,%s,%s%s,%s,%s,%s,%s,%s)'
+                select = 'SELECT * FROM fecha WHERE fecha = %s'
+                cursor.execute(select, (file[:-5], ))
+                id_fecha = 0
+                rows = cursor.fetchall()
+                if len(rows) == 0:
 
-                     cursor.execute(insert, (casa['titulo'],casa['precio'],casa['m2'],casa['recamaras'],casa['wc'],casa['cars'],casa['descripcion'],casa['tipo'],1,1,id_colonia,id_fecha))
-                     conexion.commit()
+                    insert = 'INSERT INTO fecha(fecha) VALUES(%s)'
+                    cursor.execute(insert, (file[:-5], ))
+                    conexion.commit()
+                    id_fecha = cursor.lastrowid
 
+                else:
+                    id_fecha = rows[0][0]
 
+                print(id_colonia)
+                insert = 'INSERT INTO bienraiz(titulo, precio, m2, rooms, baths, cars, descripcion, id_tipo, id_origen, id_colonia, id_fecha) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+                cursor.execute(insert, (casa['titulo'],
+                                            casa['precio'],
+                                            casa['m2'],
+                                            casa['recamaras'],
+                                            casa['wc'],
+                                            casa['cars'],
+                                            casa['descripcion'],
+                                            tipo[casa['tipo']],
+                                            1,
+                                            id_colonia,
+                                            id_fecha))
 
+                conexion.commit()
